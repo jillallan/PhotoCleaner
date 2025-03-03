d02520c3c98873010ae1c0ba70f776b10ef1f81e
Jill Allan <54241079+jillallan@users.noreply.github.com>
2025-03-03 20:33:40 +0000

    Initial Commit

---
+++ PhotoCleanerTests/PhotoCleanerTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCleanerTests.swift
+//  PhotoCleanerTests
+//
+//  Created by Jill Allan on 03/03/2025.
+//
+
+import Testing
+
+struct PhotoCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
